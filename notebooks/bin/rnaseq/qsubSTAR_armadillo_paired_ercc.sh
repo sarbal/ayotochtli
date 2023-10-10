635d7c5efe4446186576adcb3ebdc668f5737661
sarbal <sarahballouz@gmail.com>
2023-10-11 05:16:53 +1100

    Add files via upload

---
+++ notebooks/bin/rnaseq/qsubSTAR_armadillo_paired_ercc.sh
@@ -0,0 +1,17 @@
+dirFASTQ=$1
+#filesIn=`cd $dirFASTQ; ls *_1.fastq`
+#filesIn=`cd $dirFASTQ; ls *_R1_001.fastq`
+#filesIn=`cd $dirFASTQ; ls *_R1.fastq`
+filesIn=`cd $dirFASTQ; ls *_1.fastq`
+filesN=`echo $filesIn | wc -w`
+nM=999
+
+optEnc="--outFilterType BySJout   --outFilterMultimapNmax 20   --outFilterMismatchNoverReadLmax 0.04   --outFilterMatchNminOverLread 0 \
+ --alignIntronMin 20   --alignIntronMax 1000000   --alignMatesGapMax 1000000   --alignSJoverhangMin 8   --alignSJDBoverhangMin 1   --sjdbScore 1 \
+ --outFilterScoreMinOverLread 0.66  --outFilterMismatchNmax $nM  --outSAMtype BAM Unsorted  --quantMode GeneCounts  \
+ --twopassMode Basic  --twopass1readsN -1 "
+
+qsub -j y -v PATH -pe threads 10 -l tmp_free=200G,m_mem_free=20G -cwd -t 1-$filesN \
+/sonas-hs/gillis/hpc/home/sballouz/subSTAR_armadillo_paired_ercc.sh \
+$dirFASTQ \"$filesIn\" ./ \
+\" $optEnc \"
