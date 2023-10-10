3711d597a7eb98404a8fe8fd9faa134d692f046b
sarbal <sarahballouz@gmail.com>
2023-10-11 05:20:04 +1100

    Update qsubSTAR_armadillo_paired_vcf.sh

--- notebooks/bin/rnaseq/qsubSTAR_armadillo_paired_vcf.sh
+++ notebooks/bin/rnaseq/qsubSTAR_armadillo_paired_vcf.sh
@@ -1,9 +1,6 @@
 dirFASTQ=$1
 vcf=$2
-#filesIn=`cd $dirFASTQ; ls *_1.fastq`
 filesIn=`cd $dirFASTQ; ls *_R1_001.fastq*`
-#filesIn=`cd $dirFASTQ; ls *001.fastq.filtered_pairs_R1.fastq*`
-#filesIn=`cd $dirFASTQ; ls *filtered_pairs_R1.fastq*`
 filesN=`echo $filesIn | wc -w`
 nM=999
 echo HERE 
@@ -11,13 +8,10 @@ optEnc="--outFilterType BySJout   --outFilterMultimapNmax 20   --outFilterMismat
  --alignIntronMin 20   --alignIntronMax 1000000   --alignMatesGapMax 1000000   --alignSJoverhangMin 8   --alignSJDBoverhangMin 1   --sjdbScore 1 \
  --outFilterScoreMinOverLread 0.66  --outFilterMismatchNmax $nM  --outSAMtype BAM Unsorted  --quantMode GeneCounts  \
  --twopassMode Basic  --twopass1readsN -1  "
-echo THERE 
 echo $vcf
 echo $filesN 
 
 qsub -j y -v PATH -pe threads 10 -l tmp_free=200G,m_mem_free=10G -cwd -t 1-$filesN \
-/sonas-hs/gillis/hpc/home/sballouz/subSTAR_armadillo_paired_genome.sh \
+subSTAR_armadillo_paired_genome.sh \
 $vcf $dirFASTQ \"$filesIn\" ./ \
 \" $optEnc \"
-
-echo WHERE 
